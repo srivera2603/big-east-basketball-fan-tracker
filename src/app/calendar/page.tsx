fb98df2a17c2fb4d2d93411070a634a4c92310ce
Sebastian Rivera <srivera2603@gmail.com>
2025-10-27 16:42:26 -0400

    Base Calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,7 +1,42 @@
+'use client';
+import CalendarGrid from '@/components/calendar/CalendarGrid';
+import MonthNav from '@/components/calendar/MonthNav';
+import { useState } from 'react';
+
 export default function Calendar() {
+  const today = new Date();
+  const [visibleMonth, setVisibleMonth] = useState(today.getMonth());
+  const [visibleYear, setVisibleYear] = useState(today.getFullYear());
+
+  const goPrevMonth = () => {
+    if (visibleMonth === 0) {
+      setVisibleMonth(11);
+      setVisibleYear(visibleYear - 1);
+    } else {
+      setVisibleMonth(visibleMonth - 1);
+    }
+  };
+
+  const goNextMonth = () => {
+    if (visibleMonth === 11) {
+      setVisibleMonth(0);
+      setVisibleYear(visibleYear + 1);
+    } else {
+      setVisibleMonth(visibleMonth + 1);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen items-center justify-center bg-blue-50">
-      <h1 className="text-4xl font-heading text-blue-900">Calendar</h1>
+    <main className="flex flex-col min-h-screen bg-blue-50">
+      <div className="flex flex-col items-center justify-center">
+        <MonthNav
+          visibleMonth={visibleMonth}
+          visibleYear={visibleYear}
+          onPrev={goPrevMonth}
+          onNext={goNextMonth}
+        />
+        <CalendarGrid visibleMonth={visibleMonth} visibleYear={visibleYear} today={today} />
+      </div>
     </main>
   );
 }
---
+++ src/components/calendar/MonthNav.tsx
@@ -0,0 +1,70 @@
+interface MonthNavProps {
+  visibleMonth: number;
+  visibleYear: number;
+  onPrev: () => void;
+  onNext: () => void;
+  minMonth: number; // Boundaries to keep people in season
+  maxMonth: number;
+}
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export default function MonthNav({
+  visibleMonth,
+  visibleYear,
+  onPrev,
+  onNext,
+  minMonth = 9, // October
+  maxMonth = 3, // April
+}: MonthNavProps) {
+  const prevDisabled = visibleMonth === minMonth;
+  const nextDisabled = visibleMonth === maxMonth;
+
+  return (
+    <div className="bg-blue-900 shadow-md rounded-t-lg px-4 py-0 mt-8 flex flex-col items-center w-full max-w-5xl">
+      {/* Month/Year and buttons*/}
+      <div className="flex items-center justify-between w-full max-w-5xl px-4 mt-6 mb-4">
+        <button
+          onClick={onPrev}
+          className={`px-2 py-1 rounded ${prevDisabled ? 'bg-gray-100 opacity-50 cursor-not-allowed' : 'text-white bg-blue-700'}`}
+          disabled={prevDisabled}
+        >
+          Prev
+        </button>
+        <h2 className="text-gray-200 text-2xl font-bold">
+          {months[visibleMonth]} {visibleYear}
+        </h2>
+        <button
+          onClick={onNext}
+          className={`px-2 py-1 rounded ${nextDisabled ? 'bg-gray-100 opacity-50 cursor-not-allowed' : 'text-white bg-blue-700'}`}
+          disabled={nextDisabled}
+        >
+          Next
+        </button>
+      </div>
+      {/* Days of the week */}
+      <div className="grid grid-cols-7 gap-3 w-full max-w-5xl">
+        {weekDays.map((day) => (
+          <div key={day} className="text-center font-semibold text-gray-200">
+            {day}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
